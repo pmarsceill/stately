079374bc6b668f20afa72006bb986736dcced598
Patrick Marsceill <patrick.marsceill@gmail.com>
2017-01-16 22:19:33 -0500

    Mobile site nav

--- assets/js/stately.js
+++ assets/js/stately.js
@@ -1,36 +1,50 @@
-function toggleNav() {
-  nav = document.getElementsByClassName('js-side-nav');
-  navItems = document.getElementsByClassName('js-side-nav-item');
-  activeLink = document.getElementsByClassName('js-side-nav-link-active');
+function toggleMainNav() {
+  var nav = document.getElementsByClassName('js-site-nav-list');
+  var navItems = document.getElementsByClassName('js-site-nav-item');
+  var navTrigger = document.getElementsByClassName('js-site-nav-trigger');
 
-  activeLink[0].addEventListener('click', function(event){
-    event.preventDefault();
-    toggleClasses();
-  });
+  if (navTrigger.length > 0) {
+    navTrigger[0].addEventListener('click', function(event){
+      event.preventDefault();
+      toggleClasses(nav[0], 'open');
+      this.classList.toggle('open')
+    });
+  }
+}
 
-  function toggleClasses(){
-    el = nav[0];
-    className = 'open';
+function toggleSideNav() {
+  var nav = document.getElementsByClassName('js-side-nav');
+  var navItems = document.getElementsByClassName('js-side-nav-item');
+  var activeLink = document.getElementsByClassName('js-side-nav-link-active');
 
-    if (el.classList) {
-      el.classList.toggle(className);
-    } else {
-      var classes = el.className.split(' ');
-      var existingIndex = classes.indexOf(className);
+  if (activeLink.length > 0) {
+    activeLink[0].addEventListener('click', function(event){
+      event.preventDefault();
+      toggleClasses(nav[0], 'open');
+    });
+  }
+}
 
-      if (existingIndex >= 0)
-        classes.splice(existingIndex, 1);
-      else
-        classes.push(className);
-      el.className = classes.join(' ');
-    }
+function toggleClasses(el, className){
+  if (el.classList) {
+    el.classList.toggle(className);
+  } else {
+    var classes = el.className.split(' ');
+    var existingIndex = classes.indexOf(className);
+
+    if (existingIndex >= 0)
+      classes.splice(existingIndex, 1);
+    else
+      classes.push(className);
+    el.className = classes.join(' ');
   }
 }
 
 // Document ready
 var ready = function(){
   // Handler when the DOM is fully loaded
-  toggleNav();
+  toggleMainNav();
+  toggleSideNav();
 };
 
 if (
