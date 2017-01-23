aa2cc8b315f5cfd4a67d7821a4fa8dd93dc61c88
Patrick Marsceill <patrick.marsceill@gmail.com>
2017-01-23 16:30:44 -0500

    Refactor js

--- assets/js/stately.js
+++ assets/js/stately.js
@@ -1,36 +1,33 @@
 function toggleMainNav() {
-  var nav = document.getElementsByClassName('js-site-nav-list');
-  var navItems = document.getElementsByClassName('js-site-nav-item');
-  var navTrigger = document.getElementsByClassName('js-site-nav-trigger');
+  const nav = document.querySelector('.js-site-nav-list');
+  const navItems = document.querySelector('.js-site-nav-item');
+  const navTrigger = document.querySelector('.js-site-nav-trigger');
 
-  if (navTrigger.length > 0) {
-    navTrigger[0].addEventListener('click', function(event){
-      event.preventDefault();
-      toggleClasses(nav[0], 'open');
-      this.classList.toggle('open')
-    });
-  }
+  navTrigger.addEventListener('click', function(event){
+    event.preventDefault();
+    toggleClasses(nav, 'open');
+    this.classList.toggle('open')
+  });
+
 }
 
 function toggleSideNav() {
-  var nav = document.getElementsByClassName('js-side-nav');
-  var navItems = document.getElementsByClassName('js-side-nav-item');
-  var activeLink = document.getElementsByClassName('js-side-nav-link-active');
+  const nav = document.querySelector('.js-side-nav');
+  const navItems = document.querySelector('.js-side-nav-item');
+  const activeLink = document.querySelector('.js-side-nav-link-active');
 
-  if (activeLink.length > 0) {
-    activeLink[0].addEventListener('click', function(event){
-      event.preventDefault();
-      toggleClasses(nav[0], 'open');
-    });
-  }
+  activeLink.addEventListener('click', function(event){
+    event.preventDefault();
+    toggleClasses(nav, 'open');
+  });
 }
 
 function toggleClasses(el, className){
   if (el.classList) {
     el.classList.toggle(className);
   } else {
-    var classes = el.className.split(' ');
-    var existingIndex = classes.indexOf(className);
+    const classes = el.className.split(' ');
+    const existingIndex = classes.indexOf(className);
 
     if (existingIndex >= 0)
       classes.splice(existingIndex, 1);
